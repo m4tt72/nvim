af04fdaabbecdc1c30b310ebf373b43bf50f3644
Yassine Fathi <yassine.fathi@trustedfamily.net>
2022-02-18 02:31:39 +0100

    chore: ui improvements

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -6,7 +6,7 @@ local options = {
   swapfile = false,
   backup = false,
   number = true,
-  relativenumber = true,
+  relativenumber = false,
   writebackup = false,
   smarttab = true,
   cindent = true,
@@ -46,13 +46,6 @@ vim.opt.shortmess:append "c"
 
 -- automatically remove any trailing whitespace
 vim.cmd "autocmd BufWritePre * %s/\\s\\+$//e"
-
--- jump to last position
-vim.cmd [[
-  if has("autocmd")
-    au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-  endif
-]]
 
 for k, v in pairs(options) do
   vim.opt[k] = v
