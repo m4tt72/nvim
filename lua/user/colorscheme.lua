49a25ca7a2ba3d4bb957d2b9dff7af5010acf030
Yassine Fathi <yassine.fathi@trustedfamily.net>
2022-03-02 11:58:09 +0100

    chore: change coc settings and theme transparency

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -9,12 +9,13 @@ if not status_ok then
 else
   vim.g.tokyonight_style = "night"
   vim.g.tokyonight_italic_functions = true
+  vim.g.tokyonight_italic_variables = false
+  vim.g.tokyonight_transparent = true
+  vim.g.tokyonight_transparent_sidebar = true
+  vim.g.tokyonight_lualine_bold = true
   vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
   vim.cmd[[
     colorscheme tokyonight
-
-    highlight Normal guibg=none
-    highlight NonText guibg=none
   ]]
 end
